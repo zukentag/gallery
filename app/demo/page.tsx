44c4f80f9d11ee5de55147e7697b335c27565c40
Rahul Raturi <rahulraturi2002@gmail.com>
2024-06-04 17:31:15 +0530

    initialize selectable grid component

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,156 +1,80 @@
 "use client";
-import React, { ReactElement, useEffect, useRef, useState } from "react";
-import components from "../components/page";
-import { Button } from "@/components/ui/Button";
+import React, { useState } from "react";
 
-interface StepsData {
-  name: string;
-  component: () => ReactElement;
-  optional: boolean;
-}
-interface CheckoutStepperProps {
-  stepsData: StepsData[];
-}
+const SelectableGrid = ({ rows, cols }: { rows: number; cols: number }) => {
+  const [selectedGrid, setSelectedGrid] = useState<number[]>([]);
+  const [isMouseDown, setIsMouseDown] = useState(false);
 
-const CheckoutComponentsData: StepsData[] = [
-  {
-    name: "Step 1",
-    component: () => <div>This is Step 1 component</div>,
-    optional: false,
-  },
-  {
-    name: "Step 2",
-    component: () => <div>This is Step 2 component</div>,
-    optional: true,
-  },
-  {
-    name: "Step 3",
-    component: () => <div>This is Step 3 component</div>,
-    optional: false,
-  },
-];
-const CheckoutStepper: React.FC<CheckoutStepperProps> = ({ stepsData }) => {
-  const [currentStep, setCurrentStep] = useState(1);
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [margins, setMargins] = useState({
-    marginLeft: 0,
-    marginRight: 0,
-  });
-  const stepRef = useRef<(HTMLDivElement | null)[]>([]);
+  const handleMouseDown = (boxNum: number) => {
+    setIsMouseDown(true);
+    setSelectedGrid([boxNum]);
+  };
+  const handleMouseUp = () => {
+    setIsMouseDown(false);
+  };
+  const handleMouseEnter = (boxNum: number) => {
+    if (isMouseDown) {
+      const startBox = selectedGrid[0];
+      const endBox = boxNum;
 
-  const ActiveComponenet = stepsData[currentStep - 1]
-    ? stepsData[currentStep - 1].component
-    : () => <></>;
+      const minRow = Math.min(
+        Math.floor((startBox - 1) / cols),
+        Math.floor((endBox - 1) / cols)
+      );
+      const maxRow = Math.max(
+        Math.floor((startBox - 1) / cols),
+        Math.floor((endBox - 1) / cols)
+      );
+      const minCol = Math.min(
+        Math.floor((startBox - 1) % cols),
+        Math.floor((endBox - 1) % cols)
+      );
+      const maxCol = Math.max(
+        Math.floor((startBox - 1) % cols),
+        Math.floor((endBox - 1) % cols)
+      );
 
-  const calculateProgressWidth = () => {
-    return ((currentStep - 1) / (stepsData.length - 1)) * 100;
+      const selected = [];
+      for (let i = minRow; i <= maxRow; i++) {
+        for (let j = minCol; j <= maxCol; j++) {
+          selected.push(i + cols + j + 1);
+        }
+      }
+      setSelectedGrid(selected);
+    }
   };
 
-  useEffect(() => {
-    setMargins({
-      marginLeft: Number(stepRef.current[0]?.offsetWidth) / 2,
-      marginRight:
-        Number(stepRef.current[stepsData.length - 1]?.offsetWidth) / 2,
-    });
-    console.log(Number(stepRef.current[0]?.offsetWidth) / 2);
-  }, [stepRef.current]);
   return (
-    <div className=" p-5 flex flex-col">
-      <div className=" flex justify-center items-center relative gap-[20%]">
-        {stepsData.map((step, ind) => {
-          return (
-            <div
-              key={step.name}
-              ref={(el) => {
-                stepRef.current[ind] = el;
-              }}
-              className="flex flex-col justify-center items-center gap-2 z-20"
-            >
-              <div
-                className={`w-10 h-10 flex justify-center items-center rounded-full border-2 z-50 ${
-                  currentStep === ind + 1
-                    ? "bg-black text-white dark:bg-white dark:text-black"
-                    : ""
-                } ${
-                  currentStep > ind + 1 || isCompleted
-                    ? "bg-green-500 text-white "
-                    : ""
-                }`}
-              >
-                {currentStep > ind + 1 || isCompleted ? `✔` : ind + 1}
-              </div>
-              <div>{step.name}</div>
-            </div>
-          );
-        })}
-
+    <div
+      className="grid gap-2 select-none"
+      style={{
+        gridTemplateRows: `repeat(${rows}, minmax(0, 1fr))`,
+        gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))`,
+      }}
+      onMouseUp={handleMouseUp}
+    >
+      {Array.from(Array(rows * cols).keys()).map((_, index) => (
         <div
-          className="absolute z-10 bg-gray-50 items-center h-2 top-5 "
-          style={{
-            width: `calc(100% - ${margins.marginLeft + margins.marginRight}%)`,
-            marginLeft: margins.marginLeft,
-            marginRight: margins.marginRight,
-          }}
+          key={index}
+          className={`flex w-8 h-8 border-2 p-5 border-black justify-center items-center ${
+            !selectedGrid.includes(index + 1) ? "bg-green-500" : "flex"
+          }`}
+          onMouseDown={() => handleMouseDown(index + 1)}
+          onMouseEnter={() => handleMouseEnter(index + 1)}
         >
-          <div
-            className={`h-full w-fit bg-green-500 transition duration-200 ease-in`}
-            style={{ width: `${calculateProgressWidth()}%` }}
-          ></div>
+          {index + 1}
         </div>
-      </div>
-      <div className="flex justify-center items-center mt-5">
-        <ActiveComponenet />
-      </div>
-      <div className="flex mt-5 justify-around">
-        {!isCompleted ? (
-          <>
-            <Button
-              disabled={currentStep === 1}
-              className={`w-20  `}
-              onClick={() => setCurrentStep((prevStep) => prevStep - 1)}
-            >
-              Previous
-            </Button>
-            <Button
-              className="w-20"
-              onClick={() => {
-                setCurrentStep((prevStep) => {
-                  if (prevStep === stepsData.length) {
-                    setIsCompleted(true);
-                    return prevStep;
-                  } else {
-                    return prevStep + 1;
-                  }
-                });
-              }}
-            >
-              Next
-            </Button>
-          </>
-        ) : (
-          <>
-            <Button
-              className="w-20"
-              onClick={() => {
-                setCurrentStep(1);
-                setIsCompleted(false);
-              }}
-            >
-              Finish
-            </Button>
-          </>
-        )}
-      </div>
+      ))}
     </div>
   );
 };
 
-const Stepper: React.FC = () => {
+const SelectableGridComponent = () => {
   return (
-    <div>
-      <CheckoutStepper stepsData={CheckoutComponentsData} />
+    <div className="p-5 flex justify-center items-center">
+      <SelectableGrid rows={10} cols={10} />
     </div>
   );
 };
 
-export default Stepper;
+export default SelectableGridComponent;
